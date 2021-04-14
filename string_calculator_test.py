21fe3f684df11d7198537a87e0773d3e4e0f26af
parastooveisi <parastoo.veisi@gmail.com>
2021-04-13 21:12:54 -0600

    Add an entry point for the test file

--- string_calculator_test.py
+++ string_calculator_test.py
@@ -70,7 +70,7 @@ class TestPartOne(unittest.TestCase):
 
     def test_parse_input_with_arbitrary_length_delimiter(self):
         self.assertEqual(StringCalculator().parse_input(
-            "1$$$2$$$3"), [1, 2, 3])
+            "1$$$2$$$3", "$$$"), [1, 2, 3])
 
     def test_extract_delimiter_without_delimiter(self):
         self.assertEqual(StringCalculator().extract_delimiter(
@@ -103,3 +103,7 @@ class TestPartOne(unittest.TestCase):
     def test_handle_negative_numbers_with_negative_inputs(self):
         self.assertRaises(Exception, StringCalculator(
         ).handle_negative_numbers, [1, -4, 5, -7])
+
+
+if __name__ == '__main__':
+    unittest.main()
